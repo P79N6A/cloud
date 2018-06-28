60d31a05ed72cbe91a0ad9188929cd79217f3738
xieshengrong <xieshengrong@homedo.com>
2018-06-28 17:23:29 +0800

    init

---
+++ cloud-eureka/src/main/java/com/springframework/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.springframework.eureka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/** @author summer 服务发现注册中心 */
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class EurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+    log.warn("服务发现注册中心 started success ");
+  }
+}
