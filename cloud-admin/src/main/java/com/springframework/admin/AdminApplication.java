8d88874794909ef302ae25610df61e6da405c43b
xieshengrong <xieshengrong@homedo.com>
2018-06-28 17:24:02 +0800

    init

---
+++ cloud-admin/src/main/java/com/springframework/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.springframework.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/** @author summer admin监控服务 */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+@Slf4j
+public class AdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdminApplication.class, args);
+    log.warn("admin监控服务 started success");
+  }
+}
