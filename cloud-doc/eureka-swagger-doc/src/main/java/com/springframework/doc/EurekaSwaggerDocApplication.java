357d16eb67de9cf7585d4706a6a2f59f8b4b8163
xieshengrong <xieshengrong@homedo.com>
2018-06-29 17:20:02 +0800

    update

--- cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
+++ cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author xieshengrong swagger 聚合文档服务
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableSwaggerButler
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.springframework.doc","com.springframework.butler.configure"})
 @Slf4j
 public class EurekaSwaggerDocApplication {
 
