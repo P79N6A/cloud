c50cd7e2b6b08081629f32343bf3b00434b04ec0
xieshengrong <xieshengrong@homedo.com>
2018-06-29 16:24:56 +0800

    update

--- cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
+++ cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
@@ -1,17 +1,22 @@
 package com.springframework.doc;
 
+import com.springframework.butler.configure.EnableSwaggerButler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
-/** @author xieshengrong */
+/**
+ * @author xieshengrong swagger 聚合文档服务
+ */
 @EnableDiscoveryClient
 @EnableSwaggerButler
 @SpringBootApplication
+@Slf4j
 public class EurekaSwaggerDocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaSwaggerDocApplication.class);
+        log.warn("swagger 聚合文档服务 started success");
     }
 }
