20484abf2eaf66a2f17b906249b3fde9d7d9bf64
xieshengrong <xieshengrong@homedo.com>
2018-08-02 17:49:05 +0800

    1master update

---
+++ common-modules/framework-utils/src/main/java/com/springframework/utils/LogTraceUtil.java
@@ -0,0 +1,40 @@
+package com.springframework.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 用来做系统出错定位跟踪
+ *
+ * @author gebiao(ge.biao@gewara.com)
+ * @since Jan 18, 2014 3:12:29 PM
+ */
+public class LogTraceUtil {
+    private final static TLogger DB_LOGGER = LoggerUtils.getLogger(LogTraceUtil.class);
+    private static List<LogTrace> traceList = new ArrayList<>();
+
+    public static void addTrace(LogTrace trace) {
+        traceList.add(trace);
+    }
+
+    public static List<String> getTraceInfo() {
+        List<String> result = new ArrayList<>();
+        for (LogTrace trace : traceList) {
+            try {
+                String ts = trace.getTrace();
+                if (StringUtils.isNotBlank(ts)) {
+                    result.add(ts);
+                }
+            } catch (Throwable e) {
+                DB_LOGGER.error("", e);
+            }
+        }
+        return result;
+    }
+
+    public static interface LogTrace {
+        String getTrace();
+    }
+}
---
+++ common-modules/framework-utils/src/main/java/com/springframework/utils/Util4Script.java
@@ -0,0 +1,9 @@
+package com.springframework.utils;
+
+/**
+ * 只用于GroovyScript改进
+ * @author summer
+ */
+public interface Util4Script {
+
+}
