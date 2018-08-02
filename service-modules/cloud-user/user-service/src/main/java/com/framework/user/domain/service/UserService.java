667e38520cffeb63355c172199e3e7d79c669655
xieshengrong <xieshengrong@homedo.com>
2018-08-02 14:13:10 +0800

    1,add user service

---
+++ service-modules/cloud-user/user-service/src/main/java/com/framework/user/domain/service/UserService.java
@@ -0,0 +1,8 @@
+package com.framework.user.domain.service;
+
+/**
+ * @author summer
+ * 2018/8/2
+ */
+public interface UserService {
+}
---
+++ service-modules/cloud-user/user-service/src/main/java/com/framework/user/domain/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.framework.user.domain.service.impl;
+
+import com.framework.user.domain.service.UserService;
+
+/**
+ * @author summer
+ * 2018/8/2
+ */
+public class UserServiceImpl implements UserService {
+}
