df1d2228346e2316e19f93952840bbafc31cda7e
xieshengrong <xieshengrong@homedo.com>
2018-08-13 16:38:23 +0800

    1master

---
+++ common-modules/framework-state-machine/src/main/java/com/springframework/state/machine/handler/EventHandler.java
@@ -0,0 +1,8 @@
+package com.springframework.state.machine.handler;
+
+/**
+ * @author summer  //TODO 暂无设计
+ * 2018/8/13
+ */
+public interface EventHandler {
+}
---
+++ common-modules/framework-state-machine/src/main/java/com/springframework/state/machine/state/StateManager.java
@@ -0,0 +1,8 @@
+package com.springframework.state.machine.state;
+
+/**
+ * @author summer  //TODO 暂无设计
+ * 2018/8/13
+ */
+public interface StateManager {
+}
