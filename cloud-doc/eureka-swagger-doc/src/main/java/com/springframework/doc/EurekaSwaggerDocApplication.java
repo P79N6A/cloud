730f745a91b9a1705c3c0da3dab9a0b830a54c1e
xieshengrong <xieshengrong@homedo.com>
2018-11-22 17:05:26 +0800

    update  doc

--- cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
+++ cloud-doc/eureka-swagger-doc/src/main/java/com/springframework/doc/EurekaSwaggerDocApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xieshengrong swagger 聚合文档服务
@@ -15,10 +17,16 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.springframework.doc","com.springframework.butler.configure"})
 @Slf4j
+@RestController
 public class EurekaSwaggerDocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaSwaggerDocApplication.class);
         log.warn("swagger 聚合文档服务 started success");
+    }
+
+    @RequestMapping("/heath")
+    public String heath(){
+        return "OK";
     }
 }
