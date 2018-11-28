b2518a82611280cc8e6aa9cbfe119f4b8e174295
xieshengrong <xieshengrong@homedo.com>
2018-11-28 19:24:19 +0800

    webflux 不支持swagger

--- cloud-gateway/gateway-api-server/src/main/java/com/springframework/gateway/web/RouteConfigController.java
+++ cloud-gateway/gateway-api-server/src/main/java/com/springframework/gateway/web/RouteConfigController.java
@@ -2,8 +2,6 @@ package com.springframework.gateway.web;
 
 import com.springframework.gateway.domain.entity.RouteConfig;
 import com.springframework.gateway.domain.service.RouteConfigService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +12,7 @@ import java.util.List;
  * @author summer
  * 2018/11/28
  */
-@Api(value = "路由配置", description = "路由配置接口")
+//@Api(value = "路由配置", description = "路由配置接口")
 @RestController
 @RequestMapping("/route/config")
 public class RouteConfigController {
@@ -31,25 +29,25 @@ public class RouteConfigController {
     }
 
     @GetMapping
-    @ApiOperation(value = "查询所有路由配置", notes = "查询所有路由配置", response = RouteConfig.class)
+//    @ApiOperation(value = "查询所有路由配置", notes = "查询所有路由配置", response = RouteConfig.class)
     public List<RouteConfig> routeConfig() {
         return routeConfigService.listAllRouteConfig();
     }
 
     @GetMapping("/serviceId/{serviceId}")
-    @ApiOperation(value = "根据serviceId查询路由配置", notes = "根据serviceId查询路由配置", response = RouteConfig.class)
+//    @ApiOperation(value = "根据serviceId查询路由配置", notes = "根据serviceId查询路由配置", response = RouteConfig.class)
     public RouteConfig getRouteConfigByServiceId(@PathVariable("serviceId") String serviceId) {
         return routeConfigService.getRouteConfigByServiceId(serviceId);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "根据Id查询路由配置", notes = "根据Id查询路由配置", response = RouteConfig.class)
+//    @ApiOperation(value = "根据Id查询路由配置", notes = "根据Id查询路由配置", response = RouteConfig.class)
     public RouteConfig getRouteConfigById(@PathVariable("id") Long id) {
         return routeConfigService.getRouteConfigById(id);
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation(value = "根据Id删除路由配置", notes = "根据Id删除路由配置")
+//    @ApiOperation(value = "根据Id删除路由配置", notes = "根据Id删除路由配置")
     public void delRouteConfigById(@PathVariable("id") Long id) {
         routeConfigService.delRouteConfigById(id);
     }
