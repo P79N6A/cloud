664dd6a20feff9c26611fb24316841e74fba2438
xie-summer <xieshengrong@live.com>
2018-12-15 16:40:33 +0800

    update  swagger

--- common-modules/framework-swgger/src/main/java/com/springframework/swgger/config/SwaggerConfig.java
+++ common-modules/framework-swgger/src/main/java/com/springframework/swgger/config/SwaggerConfig.java
@@ -1,12 +1,12 @@
 package com.springframework.swgger.config;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,21 +16,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableConfigurationProperties(value = {SwaggerConfigProperties.class})
 public class SwaggerConfig {
 
-    private static final String VERSION = "1.0.0";
+    private final SwaggerConfigProperties swaggerConfigProperties;
+
+    public SwaggerConfig(SwaggerConfigProperties swaggerConfigProperties) {
+        this.swaggerConfigProperties = swaggerConfigProperties;
+    }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger API")
-                .description("This is to show api description")
+                .title(swaggerConfigProperties.getTitle())
+                .description(swaggerConfigProperties.getDescription())
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .termsOfServiceUrl("")
-                .version(VERSION)
-                .contact(new Contact("", "", "xieshengrong@live.com"))
+                .termsOfServiceUrl(swaggerConfigProperties.getTermsOfServiceUrl())
+                .version(swaggerConfigProperties.getVersion())
+                .contact(swaggerConfigProperties.getContact())
+                .extensions(swaggerConfigProperties.getVendorExtensions())
                 .build();
     }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
