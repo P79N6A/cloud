ee83d5d49395d3c899655741d06d6176b9a8d9dc
xie-summer <xieshengrong@live.com>
2018-12-15 20:36:18 +0800

    update  gateway

--- cloud-gateway/gateway-api-server/src/main/java/com/springframework/gateway/Bootstrap.java
+++ cloud-gateway/gateway-api-server/src/main/java/com/springframework/gateway/Bootstrap.java
@@ -3,7 +3,13 @@ package com.springframework.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author summer
@@ -11,6 +17,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @SpringCloudApplication
 @Configuration
+@EnableFeignClients
+//@EnableHystrixDashboard
+@EnableTurbine
 @Slf4j
 public class Bootstrap {
 
