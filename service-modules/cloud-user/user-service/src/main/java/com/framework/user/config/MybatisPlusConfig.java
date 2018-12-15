9f9841fc6ccd5c9ae5b992efbee38d8fad7f481a
xie-summer <xieshengrong@live.com>
2018-12-16 00:58:31 +0800

    update  eureka

--- service-modules/cloud-user/user-service/src/main/java/com/framework/user/config/MybatisPlusConfig.java
+++ service-modules/cloud-user/user-service/src/main/java/com/framework/user/config/MybatisPlusConfig.java
@@ -1,43 +1,43 @@
-package com.framework.user.config;//package com.springframework.gateway.config;
-
-import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @author summer
- */
-@Configuration
-@MapperScan(basePackages = "com.framework.user.domain.*.mapper")
-public class MybatisPlusConfig {
-
-    /**
-     * mybatis-plus SQL执行效率插件【生产环境可以关闭】
-     */
-    @Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
-    }
-    /**
-     * mybatis-plus分页插件<br>
-     * 文档：http://mp.baomidou.com<br>
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        // 开启 PageHelper 的支持
-        paginationInterceptor.setLocalPage(true);
-    /*
-     * 【测试多租户】 SQL 解析处理拦截器<br>
-     * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
-     */
-    //        List<ISqlParser> sqlParserList = new ArrayList<>();
-    //        TenantSqlParser tenantSqlParser = new TenantSqlParser();
-    //        sqlParserList.add(tenantSqlParser);
-    //        paginationInterceptor.setSqlParserList(sqlParserList);
-    paginationInterceptor.setDialectType("mysql");
-        return paginationInterceptor;
-    }
-}
+//package com.framework.user.config;//package com.springframework.gateway.config;
+//
+//import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+//import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author summer
+// */
+//@Configuration
+//@MapperScan(basePackages = "com.framework.user.domain.*.mapper")
+//public class MybatisPlusConfig {
+//
+//    /**
+//     * mybatis-plus SQL执行效率插件【生产环境可以关闭】
+//     */
+//    @Bean
+//    public PerformanceInterceptor performanceInterceptor() {
+//        return new PerformanceInterceptor();
+//    }
+//    /**
+//     * mybatis-plus分页插件<br>
+//     * 文档：http://mp.baomidou.com<br>
+//     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+//        // 开启 PageHelper 的支持
+//        paginationInterceptor.setLocalPage(true);
+//    /*
+//     * 【测试多租户】 SQL 解析处理拦截器<br>
+//     * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
+//     */
+//    //        List<ISqlParser> sqlParserList = new ArrayList<>();
+//    //        TenantSqlParser tenantSqlParser = new TenantSqlParser();
+//    //        sqlParserList.add(tenantSqlParser);
+//    //        paginationInterceptor.setSqlParserList(sqlParserList);
+//    paginationInterceptor.setDialectType("mysql");
+//        return paginationInterceptor;
+//    }
+//}
