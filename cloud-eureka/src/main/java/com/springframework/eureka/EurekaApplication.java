b30d9145f77d0df23f0712fffd1fc73a50fbe523
xie-summer <xieshengrong@live.com>
2018-12-15 16:43:29 +0800

    update  eureka

--- cloud-eureka/src/main/java/com/springframework/eureka/EurekaApplication.java
+++ cloud-eureka/src/main/java/com/springframework/eureka/EurekaApplication.java
@@ -1,16 +1,18 @@
 package com.springframework.eureka;
 
+import com.springframework.swgger.config.SwaggerConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
 /** @author summer 服务发现注册中心 */
 @SpringCloudApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
+@Import(SwaggerConfig.class)
 @Slf4j
 public class EurekaApplication {
 
