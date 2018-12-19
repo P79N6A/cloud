f135463bff128845d5288620a00526809eee947c
xieshengrong <xieshengrong@homedo.com>
2018-12-19 16:02:43 +0800

    update feign

--- common-modules/framework-feign/src/main/java/com/springframework/feign/configure/FeignConfig.java
+++ common-modules/framework-feign/src/main/java/com/springframework/feign/configure/FeignConfig.java
@@ -4,6 +4,10 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.security.Principal;
 
 /**
  * @author summer
@@ -14,10 +18,13 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig implements RequestInterceptor {
     @Value("${spring.application.name}")
     private String clientServiceId;
-
+    private static final String AUTHORIZATION_HEADER = "Authorization";
     @Override
     public void apply(RequestTemplate requestTemplate) {
         requestTemplate.header("X-FeignOrigin", clientServiceId);
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        Principal principal = attributes.getRequest().getUserPrincipal();
+
     }
 
 }
---
+++ common-modules/framework-hystrix/src/main/java/com/springframework/hystrix/RequestAttributeHystrixConcurrencyStrategy.java
@@ -0,0 +1,10 @@
+package com.springframework.hystrix;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+
+/**
+ * @author summer
+ * 2018/12/19
+ */
+public class RequestAttributeHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
+}
