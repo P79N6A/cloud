6d8d84e38ab1df61353028863fb2f02bf656f1d5
xieshengrong <xieshengrong@homedo.com>
2018-12-20 20:23:49 +0800

    update cat

--- common-modules/framework-datasource/src/main/java/com/springframework/common/datasource/MyBatisPlusAutoConfiguration.java
+++ common-modules/framework-datasource/src/main/java/com/springframework/common/datasource/MyBatisPlusAutoConfiguration.java
@@ -3,38 +3,30 @@
  */
 package com.springframework.common.datasource;
 
-import org.apache.ibatis.logging.Log;
-import org.apache.ibatis.logging.LogFactory;
-import org.apache.ibatis.plugin.Interceptor;
+import com.springframework.common.datasource.configure.CatMybatisPlugin;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 
 /**
  * @author summer
  *
  */
+@Slf4j
 @Configuration
 @ConditionalOnBean(DataSource.class)
 public class MyBatisPlusAutoConfiguration {
-    private static final Log logger = LogFactory.getLog(MyBatisPlusAutoConfiguration.class);
 
-
-    private List<Interceptor> interceptors;
-
-
-    public MyBatisPlusAutoConfiguration() {
+    @Bean
+    @ConditionalOnMissingBean(CatMybatisPlugin.class)
+    public CatMybatisPlugin catMybatisPlugin() {
+        log.info(" CatMybatisPlugin  Init ");
+        return new CatMybatisPlugin();
     }
-
-//    @Bean
-//    @ConditionalOnMissingBean(CatMybatisPlugin.class)
-//    public CatMybatisPlugin catMybatisPlugin() {
-//        CatMybatisPlugin catMybatisPlugin=  new CatMybatisPlugin();
-//        interceptors.add(catMybatisPlugin);
-//        return catMybatisPlugin;
-//    }
 
 }
